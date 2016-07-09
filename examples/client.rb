08ec1ceec7d5d50b87f37161718af7cc8e5253fa
Enrico Rubboli <rubboli@gmail.com>
2016-07-09 12:15:10 +0200

    repeat test 10000 times

--- examples/client.rb
+++ examples/client.rb
@@ -3,6 +3,7 @@ require 'pry'
 require 'grenache-ruby-base'
 require_relative "../lib/grenache/base.rb"
 require_relative "../lib/grenache/websocket.rb"
+require 'benchmark'
 
 Grenache::Base.configure do |conf|
    conf.grape_address = "ws://127.0.0.1:30002"
@@ -11,9 +12,13 @@ end
 EM.run do
 
   c = Grenache::Base.new
-  c.request("test",'world') do |msg|
-    puts msg
-    EM.stop
+  d1 = Time.now
+  10000.times do |n|
+    c.request("test","world #{n}") do |msg|
+      puts msg
+    end
   end
+  d2 = Time.now
 
+  puts d2-d1
 end
